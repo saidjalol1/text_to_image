08b53400e5e52173127866259f5534c894ddfcb1
saidjalol <saidjalolturakhujayev@gmail.com>
2025-09-17 11:30:34 +0500

    Fix f-string issue in settings.py

--- configs/settings.py
+++ configs/settings.py
@@ -7,7 +7,7 @@ MEDIA_DIR = os.makedirs(os.path.join(BASE_DIR, "media"), exist_ok=True)
 
 # API confs
 API_URL = config("API_URL")
-REQUEST_HEADERS = {config("HEADER"): f"{config("TOKEN_TYPE")} {config("HUGGING_FACE_TOKEN")}"}
+REQUEST_HEADERS = {config("HEADER"): f"{config('TOKEN_TYPE')} {config('HUGGING_FACE_TOKEN')}"}
 
 
 # Bot conf
